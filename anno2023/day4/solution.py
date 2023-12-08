09f34e4354c1ded2ccb039abbd89df5279061013
Johan Pelck Olsen (JPQO) <jpqo@novonordisk.com>
2023-12-08 13:48:44 +0100

    Solve day4 part 2

--- anno2023/day4/solution.py
+++ anno2023/day4/solution.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Dict, List, Tuple
 
 LINES = Path(__file__).parent.joinpath("input.txt").read_text().splitlines()
 
@@ -11,10 +12,12 @@ class Card:
     own_numbers: list[int]
 
     @property
+    def matches(self) -> int:
+        return len(set(self.winning_numbers) & set(self.own_numbers))
+
+    @property
     def score(self) -> int:
-        if len(set(self.winning_numbers) & set(self.own_numbers)) == 0:
-            return 0
-        return 2 ** (len(set(self.winning_numbers) & set(self.own_numbers)) - 1)
+        return 2 ** (self.matches - 1) if self.matches else 0
 
 
 def parse_card(line: str) -> Card:
@@ -33,8 +36,18 @@ def part_one(lines: list[str]) -> int:
 
 
 def part_two(lines: list[str]) -> int:
-    pass
+    entries: Tuple[Dict] = tuple({"copies": 1} for _ in lines)
+    for i, line in enumerate(lines):
+        card = parse_card(line)
+        if card.matches == 0:
+            continue
+        for entry in entries[i + 1 : i + card.matches + 1]:
+            try:
+                entry["copies"] += entries[i]["copies"]
+            except KeyError:
+                pass
+    return sum([entry["copies"] for entry in entries])
 
 
 if __name__ == "__main__":
-    print(part_one(LINES))
+    print(part_two(LINES))
