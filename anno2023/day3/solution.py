c09dc1ab6e8a6c80a2a15353e1323c2f3f97bae5
Johan Pelck Olsen (JPQO) <jpqo@novonordisk.com>
2023-12-08 11:24:42 +0100

    Solve day3 part1

--- anno2023/day3/solution.py
+++ anno2023/day3/solution.py
@@ -1,10 +1,41 @@
 from pathlib import Path
+import re
 
-lines = Path(__file__).parent.joinpath("input.txt").read_text().splitlines()
+LINES = Path(__file__).parent.joinpath("input.txt").read_text().splitlines()
+SYMBOLS = {"\\" + c for c in "".join(LINES) if not c.isdigit() and c != "."}
+SYMBOLS_REGEX = r"|".join(SYMBOLS)
+NUMBER_REGEX = r"\d+"
+
+
+def find_symbols(line: str) -> set[int]:
+    return {m.start() for m in re.finditer(SYMBOLS_REGEX, line)}
 
 
 def part_one(lines: list[str]) -> int:
-    pass
+    curr_line = ""
+    prev_line = ""
+
+    part_numbers = []
+    for line in lines + [""]:
+        next_line = line
+        if not curr_line:
+            curr_line = next_line
+            continue
+
+        # Find all indexes of symbols in the current line and the one above and below
+        indexes = {
+            i for l in [prev_line, curr_line, next_line] for i in find_symbols(l)
+        }
+
+        # Find all numbers in the current line that are adjacent to a symbol
+        for m in re.finditer(NUMBER_REGEX, curr_line):
+            if any(x in indexes for x in range(m.start() - 1, m.end() + 1)):
+                part_numbers.append(int(m.group()))
+
+        # Iterate lines
+        prev_line = curr_line
+        curr_line = next_line
+    return sum(part_numbers)
 
 
 def part_two(lines: list[str]) -> int:
@@ -12,4 +43,4 @@ def part_two(lines: list[str]) -> int:
 
 
 if __name__ == "__main__":
-    pass
+    print(part_one(LINES))
--- anno2023/day3/test_sample.py
+++ anno2023/day3/test_sample.py
@@ -1,9 +1,14 @@
 from pathlib import Path
+import re
 
-from anno2023.day3.solution import part_one
+from anno2023.day3.solution import part_one, SYMBOLS, SYMBOLS_REGEX, LINES
+
+
+def test_regex():
+    assert [m.start() for m in re.finditer(SYMBOLS_REGEX, "ab+cd*")] == [2, 5]
 
 
 def test_sample_input():
     lines = Path(__file__).parent.joinpath("sample_input.txt").read_text().splitlines()
-
+    print(*lines, end="\n")
     assert part_one(lines) == 4361
