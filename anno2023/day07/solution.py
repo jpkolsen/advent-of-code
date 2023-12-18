86893d1033f481a35277e2f9c852b3b7bf202ea5
Johan Pelck Olsen (JPQO) <jpqo@novonordisk.com>
2023-12-18 21:05:20 +0100

    Solve day 7

--- anno2023/day07/solution.py
+++ anno2023/day07/solution.py
@@ -1,31 +1,44 @@
 from enum import Enum, StrEnum, auto
 from pathlib import Path
+from typing import Optional
 
 LINES = Path(__file__).parent.joinpath("input.txt").read_text().splitlines()
 
 
 class HandType(Enum):
-    FIVE_OF_A_KIND = auto()
-    FOUR_OF_A_KIND = auto()
-    FULL_HOUSE = auto()
-    THREE_OF_A_KIND = auto()
-    TWO_PAIRS = auto()
-    ONE_PAIR = auto()
     HIGH_CARD = auto()
+    ONE_PAIR = auto()
+    TWO_PAIRS = auto()
+    THREE_OF_A_KIND = auto()
+    FULL_HOUSE = auto()
+    FOUR_OF_A_KIND = auto()
+    FIVE_OF_A_KIND = auto()
 
 
 class Hand:
-    card_rank = list("23456789TJQKA")
+    def __init__(self, cards: str, bid: int, card_rank: str = "23456789TJQKA"):
+        self.cards: str = cards
+        self.bid: int = bid
+        self.card_rank: str = card_rank
+        self._hand_type: HandType = self.find_type()
 
-    def __init__(self, cards: str, bid: int):
-        self.cards = cards
-        self.bid = bid
+    @property
+    def hand_type(self):
+        return self._hand_type
+
+    @hand_type.setter
+    def hand_type(self, value: HandType):
+        self._hand_type = value
 
     def __repr__(self):
-        return self.find_type().value
+        return f"{self.cards} ({self.find_type()}) {self.bid}"
 
     def __lt__(self, other: "Hand"):
-        return self.find_type().value < other.find_type().value
+        if self.hand_type.value == other.hand_type.value:
+            for c_self, c_other in zip(self.cards, other.cards):
+                if c_self != c_other:
+                    return self.card_rank.index(c_self) < self.card_rank.index(c_other)
+        return self.hand_type.value < other.hand_type.value
 
     def find_type(self) -> HandType:
         if self.cards.count(self.cards[0]) == 5:
@@ -46,17 +59,37 @@ class Hand:
         else:
             return HandType.HIGH_CARD
 
+    def optimize_hand(self, cards: str):
+        for card in cards:
+            if card == "J":
+                other_cards = set(cards.replace(card, ""))
+                for new_card in other_cards:
+                    temp_hand = Hand(cards.replace(card, new_card), 1)
+                    if self.hand_type.value < temp_hand.hand_type.value:
+                        self.hand_type = temp_hand.hand_type
+                    temp_hand.optimize_hand(cards.replace(card, new_card))
+
 
 def rank_hands(hands: list[Hand]) -> list[Hand]:
-    return sorted(hands)
+    sorted_hands = sorted(hands)
+    return sorted_hands
 
 
 def part_one(lines: list[str]) -> int:
     hands = [Hand(line.split()[0], int(line.split()[1])) for line in lines]
+    ranked_hands = rank_hands(hands)
+    return sum(i * hand.bid for i, hand in enumerate(ranked_hands, start=1))
 
 
 def part_two(lines: list[str]) -> int:
-    return 0
+    hands = [
+        Hand(line.split()[0], int(line.split()[1]), "J23456789TQKA") for line in lines
+    ]
+    for hand in hands:
+        hand.optimize_hand(hand.cards)
+    # _ = [hand.optimize_hand(hand.cards) for hand in hands]
+    ranked_hands = rank_hands(hands)
+    return sum(i * hand.bid for i, hand in enumerate(ranked_hands, start=1))
 
 
 if __name__ == "__main__":
--- anno2023/day07/test_sample.py
+++ anno2023/day07/test_sample.py
@@ -1,22 +1,37 @@
 from pathlib import Path
 import random
 
-from .solution import Hand, HandType, part_one, part_two, rank_hands
+from anno2023.day07.solution import (
+    Hand,
+    HandType,
+    part_one,
+    part_two,
+    rank_hands,
+)
 
 LINES = Path(__file__).parent.joinpath("sample_input.txt").read_text().splitlines()
 
 
 def test_hand_types():
-    assert Hand("AAAAA", 1).find_type().value == HandType.FIVE_OF_A_KIND.value == 1
-    assert Hand("AAAA2", 1).find_type().value == HandType.FOUR_OF_A_KIND.value == 2
-    assert Hand("AAABB", 1).find_type().value == HandType.FULL_HOUSE.value == 3
+    assert Hand("AAAAA", 1).find_type().value == HandType.FIVE_OF_A_KIND.value == 7
+    assert Hand("AAAA2", 1).find_type().value == HandType.FOUR_OF_A_KIND.value == 6
+    assert Hand("AAABB", 1).find_type().value == HandType.FULL_HOUSE.value == 5
     assert Hand("AAABC", 1).find_type().value == HandType.THREE_OF_A_KIND.value == 4
-    assert Hand("AABBC", 1).find_type().value == HandType.TWO_PAIRS.value == 5
-    assert Hand("AABCD", 1).find_type().value == HandType.ONE_PAIR.value == 6
-    assert Hand("ABCDE", 1).find_type().value == HandType.HIGH_CARD.value == 7
+    assert Hand("AABBC", 1).find_type().value == HandType.TWO_PAIRS.value == 3
+    assert Hand("AABCD", 1).find_type().value == HandType.ONE_PAIR.value == 2
+    assert Hand("ABCDE", 1).find_type().value == HandType.HIGH_CARD.value == 1
 
 
-def test_ranked_hands():
+def test_compare_hand_types():
+    assert Hand("AAAAA", 1) > Hand("AAAA2", 1)
+    assert Hand("AAAA2", 1) > Hand("AAABB", 1)
+    assert Hand("AAABB", 1) > Hand("AAABC", 1)
+    assert Hand("AAABC", 1) > Hand("AABBC", 1)
+    assert Hand("AABBC", 1) > Hand("AABCD", 1)
+    assert Hand("AABCD", 1) > Hand("ABCDE", 1)
+
+
+def test_rank_hand_types():
     hands = [
         Hand("AAAAA", 1),
         Hand("AAAA2", 1),
@@ -26,6 +41,7 @@ def test_ranked_hands():
         Hand("AABCD", 1),
         Hand("ABCDE", 1),
     ]
+    hands.reverse()
     rand = random.Random()
     print(f"Shuffling hands with seed {rand.seed()}")
     shuffled_hands = rand.sample(hands, k=len(hands))
@@ -34,9 +50,58 @@ def test_ranked_hands():
     assert rank_hands(shuffled_hands) == hands
 
 
+def test_compare_cards():
+    assert Hand("AAAAA", 1) > Hand("KKKKK", 1)
+    assert Hand("KK677", 1) > Hand("KTJJT", 1)
+    assert Hand("QQQJA", 1) > Hand("T55J5", 1)
+
+
+def test_rank_card_values():
+    hands = [Hand(f"{c}{c}{c}{c}{c}", 1) for c in "23456789TJQKA"]
+    rand = random.Random()
+    print(f"Shuffling hands with seed {rand.seed()}")
+    shuffled_hands = rand.sample(hands, k=len(hands))
+
+    assert shuffled_hands != hands
+    assert rank_hands(shuffled_hands) == hands
+
+    hands = [
+        Hand("T55J5", 1),
+        Hand("QQQJA", 1),
+    ]
+    shuffled_hands = hands.copy()[::-1]
+    assert shuffled_hands != hands
+    assert rank_hands(shuffled_hands) == hands
+
+    hands = [
+        Hand("KTJJT", 1),
+        Hand("KK677", 1),
+    ]
+    shuffled_hands = hands.copy()[::-1]
+
+    assert shuffled_hands != hands
+    assert rank_hands(shuffled_hands) == hands
+
+
+def test_optimize_hand():
+    hand = Hand("T55J5", 1)
+    assert hand.hand_type == HandType.THREE_OF_A_KIND
+    hand.optimize_hand(hand.cards)
+    assert hand.hand_type == HandType.FOUR_OF_A_KIND
+
+    hand = Hand("T12J5", 1)
+    assert hand.hand_type == HandType.HIGH_CARD
+    hand.optimize_hand(hand.cards)
+    assert hand.hand_type == HandType.ONE_PAIR
+
+
 def test_part_one():
     assert part_one(LINES) == 6440
 
 
 def test_part_two():
-    assert part_two(LINES) == 0
+    assert part_two(LINES) == 5905
+
+
+if __name__ == "__main__":
+    test_part_two()
