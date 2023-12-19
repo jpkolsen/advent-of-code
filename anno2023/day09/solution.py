d95fa62878512d3f9811658a0c5d2af80648d95c
Johan Pelck Olsen (JPQO) <jpqo@novonordisk.com>
2023-12-19 22:38:08 +0100

    Started day09 - not really getting there

---
+++ anno2023/day09/solution.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from typing import List
+
+LINES = Path(__file__).parent.joinpath("input.txt").read_text().splitlines()
+
+
+class History:
+    def __init__(self):
+        self.layers = []
+
+    def find_diffs(self, sequence: List[int]) -> List[int]:
+        diffs = []
+        for i in range(len(sequence) - 1):
+            diffs.append(sequence[i + 1] - sequence[i])
+        return diffs
+
+    def compute_layer(self, sequence: List[int]):
+        while not all(seq == sequence[0] for seq in sequence):
+            diffs = self.find_diffs(sequence)
+            self.layers.append(diffs)
+            self.compute_layer(diffs)
+        return sequence[0]
+
+    def propagate_increase(self, sequence: List[int]):
+        for layer in self.layers:
+            sequence.append(sequence[-1] + layer[-1])
+        return sequence[-1]
+
+
+def part_one(lines: list[str]) -> int:
+    results = []
+    for line in lines:
+        sequence = [int(x) for x in line.split()]
+        history = History()
+        history.compute_layer(sequence)
+        results.append(history.propagate_increase(sequence))
+
+    return sum(results)
+
+
+def part_two(lines: list[str]) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    print("Part one:", part_one(LINES))
+    print("Part two:", part_two(LINES))
---
+++ anno2023/day09/test_sample.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from .solution import part_one, part_two
+
+LINES = Path(__file__).parent.joinpath("sample_input.txt").read_text().splitlines()
+
+
+def test_part_one():
+    assert part_one(LINES) == 114
+
+
+def test_part_two():
+    assert part_two(LINES) == 0
